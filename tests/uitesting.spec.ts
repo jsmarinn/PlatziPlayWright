fcc3e77aae9c3f0c90e887183b1a82a622aef9a8
Juan Sebastian Marin Naranjo <jsmarinn@gmail.com>
2025-07-09 00:36:37 -0500

    Clase de selectores clásicos

--- tests/uitesting.spec.ts
+++ tests/uitesting.spec.ts
@@ -2,7 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://uitestingplayground.com/');
-  await page.getByText('Aristotle').click();
-  await page.getByRole('link', { name: 'Resources' }).click();
-  await page.getByRole('link', { name: 'Home' }).click();
+  //await page.getByText('Aristotle').click();
+  //await page.getByRole('link', { name: 'Resources' }).click();
+  //await page.getByRole('link', { name: 'Home' }).click();
+  await page.locator('text=Aristotle').click();
+  await page.locator('a.nav-link:has-text("Resources")').click();
+  await page.locator('a.nav-link:has-text("Home")').click();
 });
